86ebb7ec2d2096e6f22cc71874635e45068860d9
alvarogonmar <alvarogonmar0@gmail.com>
2025-07-24 18:42:28 -0600

    feat: add Bootstrap and React Router for navigation
    - Added Bootstrap and React-Bootstrap for styling and components.
    - Implemented a responsive navigation bar using React-Bootstrap.
    - Set up routing with React Router, including routes for Home, About, and Projects.
    - Created a new CSS module for styling the App component.
    - Updated index.css with custom styles for the navbar.

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,10 +1,39 @@
 import "./App.css";
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <h1>My Portfolio</h1>
-    </>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <header style={{ paddingTop: "80px" }}>
+              <h1 className={styles.title}>Home</h1>
+            </header>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <header style={{ paddingTop: "80px" }}>
+              <h1 className={styles.title}>About</h1>
+            </header>
+          }
+        />
+        <Route
+          path="/projects"
+          element={
+            <header style={{ paddingTop: "80px" }}>
+              <h1 className={styles.title}>Projects</h1>
+            </header>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ my-portfolio/src/components/NavBar.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import {
+  AiOutlineHome,
+  AiOutlineUser,
+  AiOutlineFundProjectionScreen,
+} from "react-icons/ai";
+
+const NavBar: React.FC = () => {
+  return (
+    <Navbar expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <strong></strong>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/" style={{ color: "var(--success)" }}>
+              <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about" style={{ color: "var(--accent)" }}>
+              <AiOutlineUser style={{ marginBottom: "2px" }} /> About
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/projects"
+              style={{ color: "var(--warning)" }}
+            >
+              <AiOutlineFundProjectionScreen style={{ marginBottom: "2px" }} />{" "}
+              Projects
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
